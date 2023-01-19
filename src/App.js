d033b06ce3d0967a28faba0640c01208d24ae387
MamaiOksana <jayko.mamayko@gmail.com>
2023-01-20 01:01:38 +0200

    fix package.json

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
 import UserComponent from "./components/Posts/Posts";
-import {getUsers} from "./services/userServices";
 import Posts from "./components/Posts/Posts";
 import Post from "./components/Post/Post";
 
 function App() {
-
-//     let [users, setUsers] = useState([]);
-//
-//     useEffect(()=>{
-// getUsers().then(value => setUsers(value));
-//     }, []);
-
-
-
 
   return (
       <div>
           <Posts/>
           {/*<Post/>*/}
       </div>
-    // <div>
-    //
-    //     {
-    //         users.map(value => <UserComponent
-    //             key={value.id}
-    //             item={value}/>)
-    //     }
-    // </div>
   );
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,7 @@
 
 import React, {useEffect, useState} from 'react';
 import Post from "../Post/Post";
+import axios from "axios"
 
 const Posts = () => {
 
@@ -16,9 +17,9 @@ const [posts, setPosts] = useState([]);
 const [count, setCount] = useState(0);
 
 useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(value => value.json())
-        .then(value => setPosts(value))
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(value => value.data)
+        .then(value => setPosts([...value]))
 },[count])
 
 
